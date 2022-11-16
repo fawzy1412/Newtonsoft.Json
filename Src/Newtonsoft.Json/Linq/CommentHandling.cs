10ec0e6126fe3e7e8f812711a8033e510330ef67
fawzy1412 <64382308+fawzy1412@users.noreply.github.com>
2022-11-16 22:59:50 +0200

    ffdfghm

--- Src/Newtonsoft.Json/Linq/CommentHandling.cs
+++ Src/Newtonsoft.Json/Linq/CommentHandling.cs
@@ -9,7 +9,7 @@
 // copies of the Software, and to permit persons to whom the
 // Software is furnished to do so, subject to the following
 // conditions:
-//
+//iugoyttye4rewe4trwte
 // The above copyright notice and this permission notice shall be
 // included in all copies or substantial portions of the Software.
 //
